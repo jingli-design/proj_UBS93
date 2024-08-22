e610b5c9be0c7fe36384ea693f4a23b9381e8081
Jing Li <155450726+jingli-design@users.noreply.github.com>
2024-08-22 10:28:13 +0800

    Create section1.R

---
+++ code.for.figure/section1.R
@@ -0,0 +1,201 @@
+
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(org.Hs.eg.db)
+library(clusterProfiler)
+source('Pro_TNBC/paper/code/ggplot.style.R')
+
+load("Pro_TNBC/data/CCLE/CCLE.RData")
+load("Pro_TNBC/output/data/CCLE/UBS93.Rpackage/UBS93.gene.df.RData")
+
+######################################################################################################################
+#   Fig 1:Development of UBS93 gene panel.
+######################################################################################################################
+
+####fig 1b: Box plot of the mean values of TOP100 gene in different cell types of CID3963.####
+#CID3963(ER+)#
+CID3963_100genelistmean_df        <- read_csv("Pro_TNBC/output/data/scRNASeq/26_sample/top100.gene/CID3963_100genelistmean_df.csv")
+CID3963_100genelistmean_df        <- subset(CID3963_100genelistmean_df,!celltype_major=="Normal Epithelial")
+fig_1b                            <- ggplot(CID3963_100genelistmean_df,aes(x=reorder(celltype_major,gene_mean,decreasing =T),y=gene_mean))+
+  geom_boxplot(width=0.6,alpha=0.8)+ggplot.style + 
+  theme(axis.text  = element_text(size=27, face="bold"),plot.title = element_text(size = 55,hjust=0.5))+
+  geom_point(size=2,position="jitter")+
+  xlab("Celltype")+ylab("Expression")+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())+
+  scale_x_discrete("Celltype", labels = c("Cancer Epithelial" = paste("Cancer cell",sep = "\n","(n=195)"),"CAFs" = paste("CAF",sep = "\n","(n=23)"),"Myeloid" = paste("Myeloid",sep = "\n","(n=474)"),"PVL" = paste("PVL",sep = "\n","(n=28)"),"T-cells" = paste("T-cells",sep = "\n","(n=2672)"),"Endothelial" = paste("Endothelial",sep = "\n","(n=102)")))
+ggsave(fig_1b,filename = "Pro_TNBC/paper/plot/section_1/Box.plot.of.the.mean.values.of.gene.panel.in.different.celltypes.of.CID3963.pdf",height = 15,width = 20)
+
+
+####fig 1c: t-SNE plot of CCLE using all the genes#####
+ccle_info                  <- read.csv("Pro_TNBC/data/CCLE/ccle_info.csv")
+brca_meta                  <- CCLE.sample.meta[CCLE.sample.meta$cell.line.tumor.site=="BREAST",]
+brca_info                  <- ccle_info[ccle_info$CCLE_Name  %in% brca_meta$cell.line.name,]
+brca_info                  <- brca_info[brca_info$primary_disease=="Breast Cancer",]
+brca.ccle.log2.rpkm        <- CCLE.log2.rpkm.matrix[,colnames(CCLE.log2.rpkm.matrix) %in% brca_info$CCLE_Name]
+
+library(Rtsne)
+cor_matrix                      <- cor(brca.ccle.log2.rpkm,method = "spearman")
+dissimilarity_matrix            <- 1 - cor_matrix
+normalized_dissimilarity_matrix <- dissimilarity_matrix / max(dissimilarity_matrix)
+tsne_result                     <- Rtsne(normalized_dissimilarity_matrix,is_distance = T,perplexity = 15)
+plot(tsne_result$Y, col = "blue", pch = 16)
+df1                             <- as.data.frame(tsne_result$Y)
+df1$CCLE_Name                   <- rownames(normalized_dissimilarity_matrix)
+df1                             <- merge(df1,brca_info[,c(4,21)],by="CCLE_Name")
+fig_1c                          <- ggplot(df1, aes(x=V1, y=V2, color=lineage_molecular_subtype)) + geom_point(size=6)+
+  xlab("t-SNE1")+ 
+  ylab("t-SNE2")+ggplot.style+
+  theme(legend.text=element_text(size=40,face='bold'),
+        legend.title=element_text(size=40,face = 'bold'),
+        legend.key.size = unit(1.5, 'lines'),
+        legend.position = "bottom")+scale_color_manual(values = c("Red","Blue","Purple","#FFCC00","#E95790"))+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())
+ggsave(fig_1c,filename = "Pro_TNBC/paper/plot/section_1/TSNE.of.breast.cancer.cell.lines.in.CCLE.using.all.gene.pdf",width=20,height=15)
+save(tsne_result,file="Pro_TNBC/paper/data/results/section_1/tsne.of.CLLE.RData")
+
+######################################################################################################################
+#  Fig S1:Scatter plot illustrating the relationship between the average E-M ratio and the rank of 27,719 genes (a), the top 2,720 genes (b), and the top 500 genes (c)
+######################################################################################################################
+source('Pro_TNBC/paper/code/ggplot.style.R')
+library(ggplot2)
+library(quantreg)
+library(readr)
+mean_score                           <- read_csv("Pro_TNBC/output/data/scRNASeq/26_sample/score/mean_score.csv")
+mean_score                           <- mean_score[order(mean_score$S_mean,decreasing = F),]
+mean_score$rank                      <- (1: length(mean_score$S_mean))
+figS1a                              <- ggplot(mean_score,aes(x=rank,y=S_mean))+
+  geom_point(size=6,show.legend = F)+ggplot.style+xlab(paste("rank",sep = "\n","(n=27719)"))+ylab("score")+
+  scale_x_continuous(limits = c(0, 28000),breaks = seq(0,28000,5000))+
+  geom_quantile(data = mean_score, mapping = aes(
+    x = rank, y = S_mean,color="red",linewidth = 1),quantiles = 0.5,show.legend = F)+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())+geom_vline(aes(xintercept=25000,color="blue",linewidth=1))
+ggsave(figS1a,filename = "Pro_TNBC/paper/plot/method/the.rank.charts.of.S_mean.pdf",width=20,height=15)
+mean_score_2720                       <- mean_score[25000:27719,]
+mean_score_2720$rank                  <- (1: length(mean_score_2720$S_mean))
+figS1b                               <- ggplot(mean_score_2720,aes(x=rank,y=S_mean))+
+  geom_point(size=6,show.legend = F)+ggplot.style+
+  xlab(paste("rank",sep = "\n","(n=2720)"))+ylab("score")+
+  scale_x_continuous(limits = c(0, 2800),breaks = seq(0,2800,500))+ 
+  geom_quantile(data = mean_score_2720, 
+                mapping = aes(x = rank, y = S_mean,color="red",linewidth = 1),
+                quantiles = 0.5,show.legend = F)+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())+geom_vline(aes(xintercept=2220,color="blue",linewidth=1))
+ggsave(figS1b,filename = "Pro_TNBC/paper/plot/method/the.second.rank.charts.of.S_mean.pdf",width=20,height=15)
+mean_score_500                       <- mean_score_2720[2221:2720,]
+mean_score_500$rank                  <- (1: length(mean_score_500$S_mean))
+figS1c                              <- ggplot(mean_score_500,aes(x=rank,y=S_mean))+
+  geom_point(size=6,show.legend = F)+ggplot.style+
+  xlab(paste("rank",sep = "\n","(n=500)"))+ylab("score")+
+  scale_x_continuous(limits = c(0, 500),breaks = seq(0,500,100))+ 
+  geom_quantile(data = mean_score_500, 
+                mapping = aes(x = rank, y = S_mean,color="red",linewidth = 1),
+                quantiles = 0.5,show.legend = F)+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())+geom_vline(aes(xintercept=400,color="blue",linewidth = 1))
+ggsave(figS1c,filename = "Pro_TNBC/paper/plot/method/the.third.rank.charts.of.S_mean.pdf",width=20,height=15)
+top100_gene                          <- mean_score_500[401:500,]$gene
+
+
+######################################################################################################################
+#  Fig S2: Box plot of the mean values of TOP100 gene expression in different cell types
+######################################################################################################################
+
+####*Figure S2a: CID4066(ER+/HER2+)####
+CID4066_100genelistmean_df       <- read_csv("Pro_TNBC/output/data/scRNASeq/26_sample/top100.gene/CID4066_100genelistmean_df.csv")
+CID4066_100genelistmean_df       <- subset(CID4066_100genelistmean_df,celltype_major!="Normal Epithelial")
+supfig_2a                        <- ggplot(CID4066_100genelistmean_df,aes(x=reorder(celltype_major,gene_mean,decreasing=T),y=gene_mean))+
+  geom_boxplot(width=0.6,alpha=0.8)+ggplot.style + 
+  theme(axis.text  = element_text( size=25, face="bold"),plot.title = element_text(size = 55,hjust=0.5))+
+  geom_point(size=1,position = "jitter")+
+  xlab("Celltype")+ylab("Expression")+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())+
+  scale_x_discrete("Celltype", labels = c("Cancer Epithelial" = paste("Cancer cell",sep = "\n","(n=514)"),"CAFs" = paste("CAF",sep = "\n","(n=923)"),"Myeloid" = paste("Myeloid",sep = "\n","(n=221)"),"PVL" = paste("PVL",sep = "\n","(n=630)"),"T-cells" = paste("T-cells",sep = "\n","(n=2171)"),"Endothelial" = paste("Endothelial",sep = "\n","(n=535)"),"B-cells"=paste("B-cells",sep = "\n","(n=38)")))
+ggsave(supfig_2a,filename = "Pro_TNBC/paper/plot/section_1/Box.plot.of.the.mean.values.of.gene.panel.in.different.celltypes.of.CID4066.pdf",height = 15,width = 20)
+
+####*Figure S2b: CID4495(TNBC)####
+CID4495_100genelistmean_df       <- read_csv("Pro_TNBC/output/data/scRNASeq/26_sample/top100.gene/CID4495_100genelistmean_df.csv")
+supfig_2b                        <- ggplot(CID4495_100genelistmean_df,aes(x=reorder(celltype_major,gene_mean,decreasing=T),y=gene_mean))+
+  geom_boxplot(width=0.6,alpha=0.8)+ggplot.style + 
+  theme(axis.text  = element_text( size=25, face="bold"),plot.title = element_text(size = 55,hjust=0.5))+
+  geom_point(size=1,position="jitter")+
+  xlab("Celltype")+ylab("Expression")+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())+
+  scale_x_discrete("Celltype", labels = c("Cancer Epithelial" = paste("Cancer cell",sep = "\n","(n=1177)"),"CAFs" = paste("CAF",sep = "\n","(n=232)"),"Myeloid" = paste("Myeloid",sep = "\n","(n=897)"),"PVL" = paste("PVL",sep = "\n","(n=191)"),"T-cells" = paste("T-cells",sep = "\n","(n=3504)"),"Endothelial" = paste("Endothelial",sep = "\n","(n=184)"),"B-cells"=paste("B-cells",sep = "\n","(n=773)"),"Plasmablasts"=paste("Plasmablasts",sep = "\n","(n=1020)")))
+ggsave(supfig_2a,filename = "Pro_TNBC/paper/plot/section_1/Box.plot.of.the.mean.values.of.gene.panel.in.different.celltypes.of.CID4495.pdf",height = 15,width = 20)
+
+####*Figure S2c: CID45171(HER2+)####
+library(ggplot2)
+library(gridExtra)
+library(cowplot)
+CID45171_100genelistmean_df      <- read_csv("Pro_TNBC/output/data/scRNASeq/26_sample/top100.gene/CID45171_100genelistmean_df.csv")
+supfig_2c                        <- ggplot(CID45171_100genelistmean_df,aes(x=reorder(celltype_major,gene_mean,decreasing=T),y=gene_mean))+
+  geom_boxplot(width=0.6,alpha=0.8)+ggplot.style + 
+  theme(axis.text  = element_text( size=25, face="bold"),plot.title = element_text(size = 55,hjust=0.5))+
+  geom_point(size=1,position = "jitter")+
+  xlab("Celltype")+ylab("Expression")+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())+
+  scale_x_discrete("Celltype", labels = c("Cancer Epithelial" = paste("Cancer cell",sep = "\n","(n=805)"),"CAFs" = paste("CAF",sep = "\n","(n=32)"),"Myeloid" = paste("Myeloid",sep = "\n","(n=172)"),"PVL" = paste("PVL",sep = "\n","(n=13)"),"T-cells" = paste("T-cells",sep = "\n","(n=1346)"),"Endothelial" = paste("Endothelial",sep = "\n","(n=15)"),"B-cells"=paste("B-cells",sep = "\n","(n=56)")))
+ggsave(supfig_2c,filename = "Pro_TNBC/paper/plot/section_1/Box.plot.of.the.mean.values.of.gene.panel.in.different.celltypes.of.CID45171.pdf",height = 15,width = 20)
+
+
+
+
+######################################################################################################################
+#  Fig S3: Run TSNE for all breast cancer cell lines by UBS93 gene lists using the correlation distance
+######################################################################################################################
+
+####Figure S3#####
+ccle_info                  <- read.csv("Pro_TNBC/data/CCLE/ccle_info.csv")
+brca_meta                  <- CCLE.sample.meta[CCLE.sample.meta$cell.line.tumor.site=="BREAST",]
+brca_info                  <- ccle_info[ccle_info$CCLE_Name  %in% brca_meta$cell.line.name,]
+brca_info                  <- brca_info[brca_info$primary_disease=="Breast Cancer",]
+brca_info                  <- subset(brca_info,lineage_molecular_subtype!="luminal_HER2_amp")
+brca.ccle.log2.rpkm        <- CCLE.log2.rpkm.matrix[,colnames(CCLE.log2.rpkm.matrix) %in% brca_info$CCLE_Name]
+
+
+load("Pro_TNBC/output/data/scRNASeq/26_sample/top100.gene/top100_gene.RData")
+brca.ccle.log2.rpkm.genecov             <- as.data.frame(brca.ccle.log2.rpkm)
+brca.ccle.log2.rpkm.genecov$ENSEMBL     <- rownames(brca.ccle.log2.rpkm.genecov)
+symbol                                  <- bitr(brca.ccle.log2.rpkm.genecov$ENSEMBL, fromType="ENSEMBL", toType="SYMBOL", OrgDb="org.Hs.eg.db")
+top100_gene_conv                        <- symbol[symbol$SYMBOL %in% top100_gene,]
+brca.ccle.log2.rpkm.genecov.100gene     <- merge(top100_gene_conv,brca.ccle.log2.rpkm.genecov,by="ENSEMBL")
+rown                                    <- brca.ccle.log2.rpkm.genecov.100gene$SYMBOL
+brca.ccle.log2.rpkm.genecov.100gene     <- brca.ccle.log2.rpkm.genecov.100gene[,-c(1,2)]
+rownames(brca.ccle.log2.rpkm.genecov.100gene)      <- rown
+brca.ccle.log2.rpkm.genecov.100gene                <- as.matrix(brca.ccle.log2.rpkm.genecov.100gene) %>% t()
+brca.ccle.log2.rpkm.genecov.100gene                <- t(brca.ccle.log2.rpkm.genecov.100gene)
+library(Rtsne)
+cor_matrix                      <- cor(brca.ccle.log2.rpkm.genecov.100gene,method = "spearman")
+dissimilarity_matrix            <- 1 - cor_matrix
+normalized_dissimilarity_matrix <- dissimilarity_matrix / max(dissimilarity_matrix)
+tsne_result                     <- Rtsne(normalized_dissimilarity_matrix,is_distance = T,perplexity = 15)
+plot(tsne_result$Y, col = "blue", pch = 16)
+df1                                               <- as.data.frame(tsne_result$Y)
+df1$CCLE_Name                                     <- rownames(normalized_dissimilarity_matrix)
+df1                                               <- merge(df1,brca_info[,c(4,21)],by="CCLE_Name")
+df1                                               <- within(df1,{
+  subtype <- NA
+  subtype[lineage_molecular_subtype=="basal_A"]   <- "Basal"
+  subtype[lineage_molecular_subtype=="basal_B"]   <- "Claudin_low"
+  subtype[lineage_molecular_subtype=="luminal"]   <- "Luminal"
+  subtype[lineage_molecular_subtype=="HER2_amp"]  <- "HER2_amp"
+})
+supfig_3                                          <- ggplot(df1, aes(x=V1, y=V2, color=subtype)) + geom_point(size=6)+
+  xlab("t-SNE1")+ 
+  ylab("t-SNE2")+ggplot.style+
+  theme(legend.text=element_text(size=40,face='bold'),
+        legend.title=element_text(size=40,face = 'bold'),
+        legend.key.size = unit(1.5, 'lines'),
+        legend.position = "bottom")+scale_color_manual(values = c("Red","Blue","Purple","#FFCC00","Black"))+
+  theme(panel.grid.major=element_line(colour=NA),
+        panel.grid.minor = element_blank())
+ggsave(supfig_3,filename = "Pro_TNBC/paper/plot/section_1/supfig.3.TSNE.of.breast.cancer.cell.lines.in.CCLE.using.UBS93.gene.panel.pdf",width=20,height=15)
+save(tsne_result,file="Pro_TNBC/paper/data/results/section_1/UBS93.gene.tsne.of.CLLE.RData")
